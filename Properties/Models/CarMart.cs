f5973393a066c129bff29917856effc6ffe9b484
Petr Pavlis <petrpavlis123@gmail.com>
2025-07-26 22:04:49 +0200

    Add project files.

---
+++ Properties/Models/CarMart.cs
@@ -0,0 +1,11 @@
+﻿using CarListApp.Maui.Properties.Models;
+
+namespace CarListApp.Maui.Properties.Models
+{
+    public class CarMart
+        { public int Id { get; set; }
+
+        public  List <Car> Cars { get; set; } = new List<Car>();
+    }
+}
+
---
+++ Properties/Services/CarService.cs
@@ -0,0 +1,35 @@
+﻿using Android.AdServices.AdIds;
+using CarListApp.Maui.Properties.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Android.Graphics.ColorSpace;
+
+namespace CarListApp.Maui.Properties.Services
+{
+    internal class CarService
+    {
+     public List <Car> GetCars()
+
+        {
+            return new List<Car>()
+
+            {
+                new Car
+                {
+                Id = 1, Make = "Honda", Model = "Fit", Vin= "123"
+                },
+                new Car
+                {
+                Id = 1, Make = "Honda", Model = "Fit", Vin= "124"
+                },
+                new Car
+                {
+                Id = 1, Make = "Honda"  , Model = "Fit" , Vin= "125"
+                 },
+            };
+        }
+    }
+}
